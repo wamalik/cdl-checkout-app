1b71c5769de885875ef82e77779e05b85bf23689
waqar.a <waqar.a@dnata.com>
2021-08-29 22:22:30 +0400

    changes to add product catalog and banner

---
+++ src/components/title.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+
+const TitleBanner = () => {
+    return (
+          <h1 align="center">CDL Checkout Application</h1>
+    );
+  };
+
+  export default TitleBanner;
